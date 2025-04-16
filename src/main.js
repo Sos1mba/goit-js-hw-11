6596de11b33bbe12413587a905a1c68dd959d0b2
Vlad_Deineko <vladd1904@gmail.com>
2025-04-16 15:59:02 +0300

    first update

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import { getImagesByQuery } from './pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const searchInput = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const query = searchInput.value.trim();
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    if (data.hits.length === 0) {
+      iziToast.info({
+        title: 'No Results',
+        message: 'No images found for your search.',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Something went wrong!',
+    });
+  } finally {
+    hideLoader();
+  }
+});
